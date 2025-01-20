91bde979378870eb52aedecb71e149ae6a54fec9
saul1113 <clclshtus@gmail.com>
2025-01-20 22:02:12 +0900

    나도 오키나와

--- SywHomeWork/ContentView.swift
+++ SywHomeWork/ContentView.swift
@@ -8,12 +8,24 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var japan: Bool = false
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Button(action: {
+                japan.toggle()
+            }) {
+                Text("일본")
+                    .font(.title)
+                    .frame(width: 100, height: 70)
+                    .background(Color.red)
+                    .foregroundStyle(.white)
+                    .cornerRadius(20)
+            }
+            if japan {
+                Text("나도 오키나와 가고싶다🇯🇵")
+                    .font(.system(size: 30))
+            }
+
         }
         .padding()
     }
