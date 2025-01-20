14e981cc9aa1e6e25ec8ed2a482a74d6025a0694
saul1113 <clclshtus@gmail.com>
2025-01-20 21:44:26 +0900

    Initial Commit

---
+++ SywHomeWork/SywHomeWorkApp.swift
@@ -0,0 +1,17 @@
+//
+//  SywHomeWorkApp.swift
+//  SywHomeWork
+//
+//  Created by 강희창 on 1/20/25.
+//
+
+import SwiftUI
+
+@main
+struct SywHomeWorkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
