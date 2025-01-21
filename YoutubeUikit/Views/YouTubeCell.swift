658094e8acab65525484276d310295449b2c47ba
saul1113 <clclshtus@gmail.com>
2025-01-21 22:09:48 +0900

    stash pop

--- YoutubeUikit/Views/YouTubeCell.swift
+++ YoutubeUikit/Views/YouTubeCell.swift
@@ -8,9 +8,10 @@
 import UIKit
 
 class YouTubeCell: UITableViewCell {
-    private let thumnailImageView = UIImageView()
+    private let thumbnailImageView = UIImageView()
     private let titleLabel = UILabel()
     private let channelLabel = UILabel()
+    private static let imageCache = NSCache<NSString, UIImage>()
     
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
@@ -22,32 +23,32 @@ class YouTubeCell: UITableViewCell {
     }
     
     private func setUpUI() {
-        thumnailImageView.translatesAutoresizingMaskIntoConstraints = false
+        thumbnailImageView.translatesAutoresizingMaskIntoConstraints = false
         titleLabel.translatesAutoresizingMaskIntoConstraints = false
         channelLabel.translatesAutoresizingMaskIntoConstraints = false
         
-        titleLabel.font = UIFont.boldSystemFont(ofSize: 18)
-        channelLabel.font = UIFont.boldSystemFont(ofSize: 14)
-        channelLabel.textColor = .systemGray
-        
-        contentView.addSubview(thumnailImageView)
+        titleLabel.font = UIFont.boldSystemFont(ofSize: 12)
+        channelLabel.font = UIFont.boldSystemFont(ofSize: 10)
+        channelLabel.textColor = .gray
+                
+        contentView.addSubview(thumbnailImageView)
         contentView.addSubview(titleLabel)
         contentView.addSubview(channelLabel)
         
         NSLayoutConstraint.activate([
-            thumnailImageView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 8),
-            thumnailImageView.topAnchor.constraint(equalTo: contentView.topAnchor, constant: 8),
-            thumnailImageView.widthAnchor.constraint(equalToConstant: 100),
-            thumnailImageView.heightAnchor.constraint(equalToConstant: 60),
+            thumbnailImageView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 8),
+            thumbnailImageView.topAnchor.constraint(equalTo: contentView.topAnchor, constant: 8),
+            thumbnailImageView.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -8),
+            thumbnailImageView.bottomAnchor.constraint(equalTo: contentView.bottomAnchor, constant: -40),
+            thumbnailImageView.heightAnchor.constraint(equalToConstant: 200),
+                        
+            channelLabel.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 8),
+            channelLabel.topAnchor.constraint(equalTo: thumbnailImageView.bottomAnchor, constant: 8),
             
-            titleLabel.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 8),
-            titleLabel.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -8),
-            titleLabel.topAnchor.constraint(equalTo: contentView.topAnchor, constant: 8),
+            titleLabel.trailingAnchor.constraint(equalTo: thumbnailImageView.trailingAnchor, constant: -8),
+            titleLabel.leadingAnchor.constraint(equalTo: channelLabel.trailingAnchor, constant: 8),
+            titleLabel.topAnchor.constraint(equalTo: thumbnailImageView.bottomAnchor, constant: 8),
             
-            channelLabel.leadingAnchor.constraint(equalTo: titleLabel.leadingAnchor),
-            channelLabel.trailingAnchor.constraint(equalTo: titleLabel.trailingAnchor),
-            channelLabel.topAnchor.constraint(equalTo: titleLabel.bottomAnchor, constant: 4),
-            channelLabel.bottomAnchor.constraint(equalTo: titleLabel.bottomAnchor, constant: -8)
         ])
     }
     
@@ -55,12 +56,39 @@ class YouTubeCell: UITableViewCell {
         titleLabel.text = video.snippet.title
         channelLabel.text = video.snippet.channelTitle
         
-        if let url = URL(string: video.snippet.thumbnails.medium.url) {
+        // 썸네일 이미지 로드
+        let urlString = video.snippet.thumbnails.medium.url
+        if let url = URL(string: urlString) {
             DispatchQueue.global().async {
                 do {
                     let data = try Data(contentsOf: url)
                     DispatchQueue.main.async {
-                        self.imageView?.image = UIImage(data: data)
+                        self.thumbnailImageView.image = UIImage(data: data)
+                    }
+                } catch {
+                    print("Error downloading image: \(error)")
+                }
+            }
+            
+            // 캐시 확인
+            if let cachedImage = YouTubeCell.imageCache.object(forKey: urlString as NSString) {
+                //NSCache에서 이미 다운로드된 이미지를 확인
+                //urlString을 키로 사용해 캐싱된 이미지를 가져옴
+                thumbnailImageView.image = cachedImage
+                //다운로드를 생략하고 캐싱된 이미지를 사용하여 로딩 시간을 줄임.
+                return
+            }
+            
+            // 비동기로 이미지 다운로드 및 캐싱
+            thumbnailImageView.image = UIImage(named: "placeholder")
+            DispatchQueue.global().async {
+                do {
+                    let data = try Data(contentsOf: url)
+                    if let image = UIImage(data: data) {
+                        DispatchQueue.main.async { [weak self] in
+                            self?.thumbnailImageView.image = image
+                            YouTubeCell.imageCache.setObject(image, forKey: urlString as NSString) // 캐싱
+                        }
                     }
                 } catch {
                     print("Error downloading image: \(error)")
