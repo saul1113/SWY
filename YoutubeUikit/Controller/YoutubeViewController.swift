fb0e39cdb806eb4e06490835fc344ba118bc645f
saul1113 <clclshtus@gmail.com>
2025-01-21 23:09:09 +0900

    이미지캐싱

--- YoutubeUikit/Controller/YoutubeViewController.swift
+++ YoutubeUikit/Controller/YoutubeViewController.swift
@@ -95,3 +95,28 @@ extension YoutubeViewController: UISearchBarDelegate {
         fetchVideos(keyword: keyword)
     }
 }
+
+extension YoutubeViewController: UIScrollViewDelegate {
+    func scrollViewDidScroll(_ scrollView: UIScrollView) {
+        let offsetY = scrollView.contentOffset.y    //스크롤 수직방향 위치
+        let contentHeight = scrollView.contentSize.height   //스크롤뷰의 전체 콘텐츠 높이
+        let height = scrollView.frame.size.height   //스크롤뷰의 표시되는 화면의 영역 높이
+
+        //스크롤 콘텐츠 끝부분 100pt 이내에 도달했는지, 스크롤 가능한 가장 아래위치
+        if offsetY > contentHeight - height - 100 && !loading && nextPage != nil {  //로딩중이면 추가요청 방지, 다음 페이지토큰이 있는지 확인
+            fetchVideos(keyword: searchBar.text ?? "")  //키워드를 통해 추가 동영상 데이터를 가져오는
+
+            /*
+             •    - 100의 역할:
+             •    데이터를 미리 로드하여 스크롤이 완료될 때까지 사용자에게 부드러운 경험을 제공합니다.
+             •    만약 - 100 대신 0을 사용하면, 스크롤이 완전히 끝났을 때만 데이터를 로드합니다. 이는 사용자 경험에 영향을 줄 수 있습니다.
+             •    loading 플래그:
+             •    API 호출이 끝나기 전에 또 다른 요청을 보내는 것을 방지합니다.
+             •    loading은 API 요청이 완료되면 다시 false로 설정됩니다.
+             •    nextPage:
+             •    YouTube API에서 반환되는 nextPageToken을 통해 페이지네이션을 구현합니다.
+             •    nextPage가 nil이면 더 이상 추가 데이터를 요청하지 않습니다.
+             */
+        }
+    }
+}
